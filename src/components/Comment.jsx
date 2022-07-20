32bb3dfa3cd48324b5b5f41fcda969438ad30dbc
John Rodrigues <johnrodrigues97106@gmail.com>
2022-07-20 11:25:32 -0300

    comunicação entre comonentes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment () {
+export function Comment ({content , onDeleteComment}) {
+  function handleDeleteComment () {
+    onDeleteComment()
+  }
+
   return (<div className={styles.comment}>
     <Avatar hasBorder={false} src="https://github.com/JohnRSinx.png" alt="" />
 
@@ -14,12 +18,15 @@ export function Comment () {
             <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
           </div>
 
-          <button title="Deletar comentário">
+          <button 
+          onClick={handleDeleteComment}
+          title="Deletar comentário"
+          >
             <Trash size={24} />
           </button>
         </header>
 
-        <p>Muito bom Devon, parabéns!! 👏👏</p>
+        <p>{content}</p>
       </div>
 
       <footer>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,27 +7,40 @@ import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post ({author ,publishedAt,content}) {
-  
+  //Estado que armazena e avisa para o React 
+  //se houve uma mudança para adicionar mais um comentario em tela 
   const [comments,setComments] = useState([
-        1,
-        2,
+        'Post muito bacana!!',
     ])
+  //Estado que armazenada os dados do textarea 
+  // E avisa para o o outro estado que vai acrescentar mais um comentario e houve uma mudança 
+  const [newCommentText , setNewCommentText] = useState('')
 
+  // Formatação do horário
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'as' HH:mm'h'", {
     locale : ptBR,
   },)
-
+  // Formatação do horário
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt,{
     locale : ptBR,
-    
     addSuffix:true,
-    
   })
+  // Função para alterar o um estado acrescentando novos comentarios 
+  // E guardando os que ja tinha 
   function handleCreateNewComment () {
     event.preventDefault()
-    setComments([...comments,comments.length+1])
+    setComments([...comments,newCommentText])
+    setNewCommentText('')
+    }
+  //Função para armazenar em um estado o valor do textarea
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
   }
-  
+  // Função que vai ser passada para o component filho para que possa
+  //deletar algum comentário
+  function onDeleteComment(comment) {
+    console.log('Deletar comentário');
+  }
 
   return (
     <article className={styles.post}>
@@ -39,40 +52,52 @@ export function Post ({author ,publishedAt,content}) {
             <span>{author.role}</span>
           </div>
         </div>
-      
-        <time title={publishedDateFormatted}dateTime={publishedAt.toISOString()}>
-          {publishedDateRelativeToNow}
+        <time 
+        title={publishedDateFormatted}
+        dateTime={publishedAt.toISOString()}
+        >
+        {publishedDateRelativeToNow}
         </time>
       </header>
-
-      <div className={styles.content}>
+        <div className={styles.content}>
        {content.map(line=> {
         if (line.type=== 'paragraph') {
-          return <p>{line.content}</p>
+          return <p key={line.content}>{line.content}</p>
         }else if (line.type=== 'link') {
-          return <p><a href='#'>{line.content}</a></p>
+          return <p key={line.content}><a href='#'>{line.content}</a></p>
         }
        })}
-      </div>
-
+        </div>
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea
+          name= "comment" 
+          value={newCommentText}
           placeholder='Deixe um comentário'
+          onChange={handleNewCommentChange}
         />
-        <footer>
+      <footer>
         <button 
         type="submit"
         >
-            Publicar  
-          </button>
-        </footer> 
+        Publicar  
+        </button>
+      </footer> 
       </form>
-
+      
       <div className={styles.commentList}>
-        {comments.map(comment => {
-          return <Comment/>
-        })}
+
+        {
+        comments.map(
+          comment => {
+          return <Comment 
+            key={comment} 
+            content={comment}
+            onDeleteComment={onDeleteComment}
+            />
+        })
+        }
+
       </div>
     </article>
   );
